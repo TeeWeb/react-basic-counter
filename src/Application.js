ba9b219e870cff71ee8ae98b8999cb7d4e27fdf5
tweber <tweber@xevo.com>
2019-08-13 00:45:48 -0700

    Refactored to use Container pattern

--- src/Application.js
+++ src/Application.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import Counter from './Counter';
+import CounterContainer from './CounterContainer';
 // import CounterContainer from './CounterContainer';
 // import WithCount from './WithCount';
 
@@ -9,7 +9,7 @@ export default class Application extends Component {
   render() {
     return (
       <main className="Application">
-        <Counter />
+        <CounterContainer />
       </main>
     );
   }
